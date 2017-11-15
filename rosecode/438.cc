4e0d4613157951947e9a1d9b7ac706e962c082f6
zimpha <zimpha@gmail.com>
2017-11-15 22:09:19 +0800

    sublinear solution

--- rosecode/438.cc
+++ rosecode/438.cc
@@ -1,8 +1,9 @@
-#include <cmath>
 #include <iostream>
-#include <iomanip>
+#include <cmath>
+#include <algorithm>
 #include <vector>
 #include <functional>
+#include <iomanip>
 
 using int64 = long long;
 using int128 = __int128;
@@ -14,35 +15,51 @@ std::ostream& operator << (std::ostream &os, const int128 &v) {
   return os;
 }
 
-int128 S(int64 n) {
-  int64 sn = sqrt(n);
-  std::vector<bool> mark(sn + 1);
+int128 F(int64 n) {
+  const int64 sn = static_cast<int64>(sqrt(n));
   std::vector<int64> pl;
+  std::vector<bool> mark(sn + 1);
   for (int i = 2; i <= sn; ++i) if (!mark[i]) {
     pl.push_back(i);
-    for (int j = i; j <= sn; j += i) mark[j] = 1;
+    for (int j = i + i; j <= sn; j += i) mark[j] = 1;
   }
-  auto prime_count = [&pl] (int64 n) {
+
+  auto sieve = [&] () {
+    std::vector<int> f(sn + 1, 1);
+    for (auto &&x: pl) f[x] = x % 4 == 3 ? 0 : x;
+    for (int i = 2; i <= sn; ++i) {
+      for (int j = 0, u = sn / i; j < pl.size() && pl[j] <= u; ++j) {
+        int v = i * pl[j];
+        if (i % pl[j]) f[v] = f[i] * (pl[j] % 4 == 3 ? 0 : pl[j]);
+        else {
+          f[v] = 0;
+          break;
+        }
+      }
+    }
+    return f;
+  };
+
+  auto calc_G = [&]() {
     static const int N = 1e6 + 10;
-    const int64 v = static_cast<int64>(sqrt(n));
     static int128 ssum[N][2], lsum[N][2];
     auto sum = [](int64 n, int e) {
       if (n < e) return int128(0);
       int128 k = (n - e) / 4;
       return e * (k + 1) + 4 * k * (k + 1) / 2;
     };
-    for (int64 i = 1; i <= v; ++i) {
+    for (int64 i = 1; i <= sn; ++i) {
       ssum[i][0] = sum(i, 1) - 1;
       ssum[i][1] = sum(i, 3);
       lsum[i][0] = sum(n / i, 1) - 1;
       lsum[i][1] = sum(n / i, 3);
     }
-    for (auto &&p: pl) if (p <= v && p != 2) {
-      int64 q = p * p, ed = std::min(v, n / q);
+    for (auto &&p: pl) if (p <= sn && p != 2) {
+      int64 q = p * p, ed = std::min(sn, n / q);
       int m = p % 4 / 2;
       for (int64 i = 1; i <= ed; ++i) {
         int64 d = i * p;
-        if (d <= v) {
+        if (d <= sn) {
           lsum[i][m] -= (lsum[d][0] - ssum[p - 1][0]) * p;
           lsum[i][m ^ 1] -= (lsum[d][1] - ssum[p - 1][1]) * p;
         } else {
@@ -50,49 +67,61 @@ int128 S(int64 n) {
           lsum[i][m ^ 1] -= (ssum[n / d][1] - ssum[p - 1][1]) * p;
         }
       }
-      for (int64 i = v; i >= q; --i) {
+      for (int64 i = sn; i >= q; --i) {
         ssum[i][(p % 4 / 2)] -= (ssum[i / p][0] - ssum[p - 1][0]) * p;
         ssum[i][(p % 4 / 2) ^ 1] -= (ssum[i / p][1] - ssum[p - 1][1]) * p;
       }
     }
-    std::vector<int128> ls(v + 1);
-    for (int i = 1; i <= v; ++i) {
-      ls[i] = lsum[i][0] - ssum[v][0];
+    std::vector<int128> ls(sn + 1);
+    for (int i = 1; i <= sn; ++i) {
+      ls[i] = lsum[i][0] - ssum[sn][0];
     }
     return ls;
   };
 
-  auto sum = prime_count(n);
-  int m = 0;
-  for (auto &&p: pl) {
-    if (p % 4 == 1) pl[m++] = p;
-  }
-  pl.resize(m);
-  std::function<int128(int, int64, int64)> dfs = [&](int d, int64 now, int64 upp) {
-    int128 ret = now;
-    if (now <= sn) {
-      ret += now * sum[now];
+  auto calc_F = [&](const std::vector<int64> &sf) {
+    std::vector<int64> val(sn * 2);
+    std::vector<int128> sum(sn * 2, 1);
+    for (int i = 1; i <= sn; ++i) val[i - 1] = i;
+    for (int i = sn; i >= 1; --i) val[sn - i + sn] = n / i;
+    int ip = 0;
+    for (auto &&v: val) {
+      while (ip < pl.size() && pl[ip] <= v / pl[ip]) ++ip;
+      v = ip - 1;
     }
-    if (upp >= 2) {
-      ret += now * 2;
-      if (now * 2 <= sn) {
-        ret += now * 2 * sum[now * 2];
+    for (int i = pl.size() - 1; i >= 0; --i) if (pl[i] % 4 != 3) {
+      for (int j = sn * 2 - 1; j >= 0; --j) {
+        if (val[j] < i) break;
+        int64 x = (j < sn ? j + 1 : n / (sn * 2 - j)) / pl[i];
+        if (x) {
+          int k = x <= sn ? x - 1 : 2 * sn - n / x;
+          int l = pl[std::max<int64>(i, val[k])], r = std::min<int64>(x, sn);
+          sum[j] += (sum[k] + (l < r ? sf[r] - sf[l] : 0)) * pl[i];
+        }
       }
     }
-    for (int i = d; i < m; ++i) {
-      if (pl[i] <= upp) {
-        ret += dfs(i + 1, now * pl[i], upp / pl[i]);
-      } else break;
-    }
-    return ret;
+    return sum.back();
   };
-  return dfs(0, 1, n) - 1;
+
+  std::vector<int64> sf(sn + 1);
+  std::vector<int> f = sieve();
+  for (auto &&x: pl) sf[x] = f[x];
+  for (int i = 1; i <= sn; ++i) {
+    sf[i] = sf[i - 1] + sf[i];
+  }
+  std::vector<int128> G = calc_G();
+  int128 ret = 0;
+  for (int i = 1; i <= sn; ++i) {
+    ret += f[i] * G[i];
+  }
+  ret += calc_F(sf);
+  return ret - 1;
 }
 
 int main() {
-  std::cout << S(10) << std::endl;
-  std::cout << S(1000) << std::endl;
-  std::cout << S(1000000) << std::endl;
-  std::cout << S(1000000000000) << std::endl;
+  std::cout << F(10) << std::endl;
+  std::cout << F(1000) << std::endl;
+  std::cout << F(1000000) << std::endl;
+  std::cout << F(1000000000000ll) << std::endl;
   return 0;
 }
